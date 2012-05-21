448fd70397456cd07880db489b5172b3542f8d1a
Reto Buerki <reet@codelabs.ch>
2012-05-21 18:18:18 +0200

    Add tkm_ prefix to IKE client exports

--- interfaces/ada/tkmrpc-interfaces-ike-client.ads
+++ interfaces/ada/tkmrpc-interfaces-ike-client.ads
@@ -3,18 +3,18 @@ with TKMRPC.Nonces;
 package TKMRPC.Client
 is
    procedure Init;
-   pragma Export (C, Init, "init");
+   pragma Export (C, Init, "tkm_init");
    --  Init client ORB.
 
    procedure Nc_Reset (Nonce_Id : Nonces.Nonce_Id_Type);
-   pragma Export (C, Nc_Reset, "nc_reset");
+   pragma Export (C, Nc_Reset, "tkm_nc_reset");
    --  Reset a NC context.
 
    function Nc_Create
      (Nonce_Id     : Nonces.Nonce_Id_Type;
       Nonce_Length : Nonces.Nonce_Length_Type)
       return Nonces.Nonce_Type;
-   pragma Export (C, Nc_Create, "nc_create");
+   pragma Export (C, Nc_Create, "tkm_nc_create");
    --  Create a nonce.
 
    RPC_Error : exception;
