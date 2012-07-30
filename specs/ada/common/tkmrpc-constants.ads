02bc1e33110b643750618195c06434af361e68b3
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2012-07-30 18:51:09 +0200

    Update to latest generated specs
    - Add Modp_4096 DH algorithm constant

--- specs/ada/common/tkmrpc-constants.ads
+++ specs/ada/common/tkmrpc-constants.ads
@@ -5,4 +5,6 @@ package Tkmrpc.Constants is
    Cfg_Version : constant Types.Version_Type := 16#0000000000000000#;
    Ike_Version : constant Types.Version_Type := 16#0000000000000000#;
 
+   Modp_4096 : constant Types.Dh_Algorithm_Type := 16#0000000000000010#;
+
 end Tkmrpc.Constants;
